13b6e4a4c5fd0c4247862e76fb089bf417842db0
pappman <emersonpapp@gmail.com>
2020-09-24 13:47:43 -0300

    first commit

---
+++ update-linux-sandbox.sh
@@ -0,0 +1,4 @@
+# needed if you build on NFS!
+sudo cp out/Debug/chrome_sandbox /usr/local/sbin/chrome-devel-sandbox
+sudo chown root:root /usr/local/sbin/chrome-devel-sandbox
+sudo chmod 4755 /usr/local/sbin/chrome-devel-sandbox
